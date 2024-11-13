4832c78994ba245999d7a65bdf3357a3368c7b44
awaisnazir08 <owaisnazir2228@gmail.com>
2024-11-13 10:12:32 +0500

    using webhook, example new user create and then using webhook, send the data of the new user to my database

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -1,7 +1,25 @@
 // /api/clerk/webhook
 
+import { db } from "@/server/db"
+
 export const POST = async (req: Request) => {
     const { data } = await req.json()
     console.log("Clerk webhook received", data)
-    return new Response("Webhook received", {status: 200})
+    const emailAddress = data.email_addresses[0].email_address
+    const firstName = data.first_name
+    const lastName = data.last_name
+    const imageUrl = data.image_url
+    const id = data.id
+
+    await db.user.create({
+        data: {
+            id: id,
+            imageUrl: imageUrl,
+            emailAddress: emailAddress,
+            firstName: firstName,
+            lastName: lastName
+        }
+})
+    console.log("User created")
+    return new Response("Webhook received", { status: 200 })
 }
