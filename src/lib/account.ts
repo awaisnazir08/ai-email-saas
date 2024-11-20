e6f33d182c0908640dfdabf5e514aa27f9201dcd
awaisnazir08 <owaisnazir2228@gmail.com>
2024-11-20 10:55:37 +0500

    complete performInitialSync function

--- src/lib/account.ts
+++ src/lib/account.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { SyncResponse, SyncUpdatedResponse } from "./types";
+import { EmailMessage, SyncResponse, SyncUpdatedResponse } from "./types";
 
 export class Account {
     private token: string;
@@ -51,14 +51,43 @@ export class Account {
             }
 
             //get the bookmark delta token
-            let storeDeltaToken: string = syncResponse.syncUpdatedToken
+            let storedDeltaToken: string = syncResponse.syncUpdatedToken
 
-            let updatedResponse = await this.getUpdatedEmails(storeDeltaToken)
+            let updatedResponse = await this.getUpdatedEmails({deltaToken: storedDeltaToken})
 
+            if (updatedResponse.nextDeltaToken) {
+                // sync has completed 
+                storedDeltaToken = updatedResponse.nextDeltaToken
+            }
 
+            let allEmails: EmailMessage[] = updatedResponse.records   // record is a list of email messages
+
+            // fetch all pages if there are more
+            while (updatedResponse.nextPageToken) {
+                updatedResponse = await this.getUpdatedEmails({pageToken: updatedResponse.nextPageToken})
+                allEmails = allEmails.concat(updatedResponse.records)
+
+                if (updatedResponse.nextDeltaToken) {
+                    // sync has ended
+                    storedDeltaToken = updatedResponse.nextDeltaToken
+                }
+            }
+            console.log("Initial sync completed, we have synced", allEmails.length, "emails")
+            
+            // store the latest delta token for future incremental sync
+
+            return {
+                emails: allEmails,
+                deltaToken: storedDeltaToken
+            }
 
         } catch(error) {
-
+            if (axios.isAxiosError(error)) {
+                console.error("Error during sync: ", JSON.stringify(error.response?.data, null, null, 2));
+            }
+            else {
+                console.error("Error during sync: ", error)
+            }
         }
     }
 
