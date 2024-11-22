3e7a695a59ce38c0c5b4c05347e8fb63a3148e50
awaisnazir08 <owaisnazir2228@gmail.com>
2024-11-22 18:01:22 +0500

    create componentt mal.tsx (incomplete)

---
+++ src/app/mail/mail.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {ResizableHandle, ResizablePanel, ResizablePanelGroup} from '@/components/ui/resizable'
+import { TooltipProvider } from '@/components/ui/tooltip'
+
+
+type Props = {
+    defaultLayout: number[] | undefined
+    navCollapsedSize: number
+}
+
+const Main = ({defaultLayout = [20,32,48], navCollapsedSize}: Props) => {
+  return (
+    <TooltipProvider delayDuration={0}>
+        <ResizablePanelGroup direction='horizontal' onLayout={(sizes: number[]) =>{
+            console.log(sizes)
+        }} className='items-stretch h-full min-h-screen'>
+            <ResizablePanel defaultSize={defaultLayout[0]} collapsedSize={navCollapsedSize}
+            collapsible={true}
+            >
+
+            </ResizablePanel>
+
+        </ResizablePanelGroup>
+    </TooltipProvider>
+  )
+}
+
+export default Main
