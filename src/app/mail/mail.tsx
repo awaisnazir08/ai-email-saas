b84aca0f9a2292e4b56334a551661cc6dc34a1b6
awaisnazir08 <owaisnazir2228@gmail.com>
2024-11-28 03:14:18 +0500

    create skeleton ui for the mail dashboard page

--- src/app/mail/mail.tsx
+++ src/app/mail/mail.tsx
@@ -1,26 +1,87 @@
-import React from 'react'
-import {ResizableHandle, ResizablePanel, ResizablePanelGroup} from '@/components/ui/resizable'
+"use client"
+
+import React, { useState } from 'react'
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable'
 import { TooltipProvider } from '@/components/ui/tooltip'
+import { cn } from '@/lib/utils'
+import { Separator } from '@/components/ui/separator'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 type Props = {
-    defaultLayout: number[] | undefined
-    navCollapsedSize: number
+  defaultLayout: number[] | undefined
+  navCollapsedSize: number
+  defaultCollapsed: boolean
 }
 
-const Main = ({defaultLayout = [20,32,48], navCollapsedSize}: Props) => {
+const Mail = ({ defaultLayout = [20, 32, 48], navCollapsedSize }: Props) => {
+
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
   return (
     <TooltipProvider delayDuration={0}>
-        <ResizablePanelGroup direction='horizontal' onLayout={(sizes: number[]) =>{
-            console.log(sizes)
-        }} className='items-stretch h-full min-h-screen'>
-            <ResizablePanel defaultSize={defaultLayout[0]} collapsedSize={navCollapsedSize}
-            collapsible={true}
-            >
-            </ResizablePanel>
+      <ResizablePanelGroup direction='horizontal' onLayout={(sizes: number[]) => {
+        console.log(sizes)
+      }} className='items-stretch h-full min-h-screen'>
+        <ResizablePanel defaultSize={defaultLayout[0]} collapsedSize={navCollapsedSize}
+          collapsible={true}
+          minSize={15}
+          maxSize={40}
+          onCollapse={() => {
+            setIsCollapsed(true)
+          }}
 
-        </ResizablePanelGroup>
+          onResize={() => {
+            setIsCollapsed(false)
+          }}
+          className={cn(isCollapsed && 'min-w-[50px] transition-all duration-300 ease-in-out')}
+        >
+          <div className='flex flex-col h-full flex-1'>
+            <div className={cn('flex h-[52px] items-center justify-between', isCollapsed ? 'h-[52px]' : 'px-2')}>
+              {/* Account Switcher */}
+              Account Switcher
+            </div>
+            <Separator />
+            {/* Sidebar */}
+            <div className="flex-1">
+              {/* AI */}
+              Ask AI
+            </div>
+          </div>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={defaultLayout[1]} minSize={30}>
+          <Tabs defaultValue='inbox'>
+            <div className='flex items-center px-4 py-2'>
+              <h1 className='text-xl font-bold'>
+                Inbox
+              </h1>
+              <TabsList className='ml-auto'>
+                <TabsTrigger value='inbox' className='text-zinc-600 dark:text-zinc-200'>
+                  Inbox
+                </TabsTrigger>
+                <TabsTrigger value='done' className='text-zinc-600 dark:text-zinc-200'>
+                  Done
+                </TabsTrigger>
+              </TabsList>
+            </div>
+            <Separator/>
+            {/* SearchBar`` */}
+            Search Bar
+            <TabsContent value='inbox'>
+              inbox
+            </TabsContent>
+            <TabsContent value='done'>
+              done
+            </TabsContent>
+          </Tabs>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={defaultLayout[2]} minSize={30}>
+          thread display
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </TooltipProvider>
   )
 }
 
-export default Main
+export default Mail
