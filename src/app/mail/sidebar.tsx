2b1fbad009afd8b7765620c60d432067e1d3dedb
awaisnazir08 <owaisnazir2228@gmail.com>
2025-01-17 21:42:40 +0500

    dynamic threads display (input, draft, sent)

--- src/app/mail/sidebar.tsx
+++ src/app/mail/sidebar.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { useLocalStorage } from 'usehooks-ts';
 import { Nav } from './nav';
 import { File, Inbox, Send } from 'lucide-react';
+import { api } from '@/trpc/react';
 
 type Props = { isCollapsed: boolean}
 
@@ -10,6 +11,18 @@ const Sidebar = ({ isCollapsed }: Props) => {
 
     const [accountId] = useLocalStorage('accountId', '')  // is also like a global storage, can access value globally 
     const [tab] = useLocalStorage<"inbox" | "draft" | "sent">("ai-email-saas-tab", "inbox")
+    const {data: inboxThreads } = api.account.getNumThreads.useQuery({
+        accountId, 
+        tab: "inbox"
+    })
+    const {data: draftThreads } = api.account.getNumThreads.useQuery({
+        accountId, 
+        tab: "draft"
+    })
+    const {data: sentThreads } = api.account.getNumThreads.useQuery({
+        accountId, 
+        tab: "sent"
+    })
     // localStorage.setItem("ai-email-saas-tab", "inbox")
   return (
     <Nav 
@@ -17,19 +30,19 @@ const Sidebar = ({ isCollapsed }: Props) => {
     links={[
         {
             title: 'Inbox',
-            label: "1",
+            label: inboxThreads?.toString() ?? '0',
             icon: Inbox,
             variant: tab === 'inbox'? "default" : "ghost"
         },
         {
             title: "Draft", 
-            label: "1",
+            label: draftThreads?.toString() ?? '0',
             icon: File,
             variant: tab === "draft" ? "default" : "ghost"
         },
         {
             title: "Sent",
-            label: "5",
+            label: sentThreads?.toString() ?? '0',
             icon: Send,
             variant: tab === "sent" ? "default" : "ghost"
         }
