82e50389a5673501fd694655b10fa43110e9326a
awaisnazir08 <owaisnazir2228@gmail.com>
2025-01-17 19:57:18 +0500

    add links in sidebar as navbar props

--- src/app/mail/sidebar.tsx
+++ src/app/mail/sidebar.tsx
@@ -1,14 +1,40 @@
 "use client";
 import React from 'react'
 import { useLocalStorage } from 'usehooks-ts';
+import { Nav } from './nav';
+import { File, Inbox, Send } from 'lucide-react';
 
 type Props = { isCollapsed: boolean}
 
 const Sidebar = ({ isCollapsed }: Props) => {
 
     const [accountId] = useLocalStorage('accountId', '')  // is also like a global storage, can access value globally 
+    const [tab] = useLocalStorage<"inbox" | "draft" | "sent">("ai-email-saas-tab", "inbox")
+    // localStorage.setItem("ai-email-saas-tab", "inbox")
   return (
-    <div>{accountId}</div>
+    <Nav 
+    isCollapsed = {isCollapsed}
+    links={[
+        {
+            title: 'Inbox',
+            label: "1",
+            icon: Inbox,
+            variant: tab === 'inbox'? "default" : "ghost"
+        },
+        {
+            title: "Draft", 
+            label: "1",
+            icon: File,
+            variant: tab === "draft" ? "default" : "ghost"
+        },
+        {
+            title: "Sent",
+            label: "5",
+            icon: Send,
+            variant: tab === "sent" ? "default" : "ghost"
+        }
+    ]}
+    ></Nav>
   )
 }
 
