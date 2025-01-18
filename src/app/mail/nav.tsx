78b713f62ed5a3e7c6faa6aef676ef3c5e1949a7
awaisnazir08 <owaisnazir2228@gmail.com>
2025-01-19 00:06:50 +0500

    fix the name of tab localStorage state

--- src/app/mail/nav.tsx
+++ src/app/mail/nav.tsx
@@ -25,7 +25,7 @@ interface NavProps {
 
 export function Nav({ links, isCollapsed }: NavProps) {
 
-    const [_, setTab] = useLocalStorage("normalhuman-tab", "inbox")
+    const [_, setTab] = useLocalStorage("ai-email-saas-tab", "inbox")
 
     return (
         <div
