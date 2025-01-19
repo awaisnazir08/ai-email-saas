31fd606be346cd8e6e3c68d73d4ceda584aae993
awaisnazir08 <owaisnazir2228@gmail.com>
2025-01-19 22:56:31 +0500

    thread list display

--- src/app/mail/thread-list.tsx
+++ src/app/mail/thread-list.tsx
@@ -1,13 +1,16 @@
 "use client"
+import DOMPurify from 'dompurify';
 import useThreads from '@/hooks/use-threads'
-import React from 'react';
-import { format } from "date-fns";
+import React, { ComponentProps } from 'react';
+import { format, formatDistanceToNow } from "date-fns";
+import { cn } from '@/lib/utils';
+import { Badge } from '@/components/ui/badge';
 
 const ThreadList = () => {
     const { threads } = useThreads();
 
     const groupedThreads = threads?.reduce((acc, thread) => {
-        const date = format(thread.emails[0]?.sentAt ?? new Date(), 'yyyy-mm-dd')
+        const date = format(thread.emails[0]?.sentAt ?? new Date(), 'yyyy-MM-dd')
         if (!acc[date]) {
             acc[date] = []
         }
@@ -16,13 +19,63 @@ const ThreadList = () => {
         return acc
     }, {} as Record<string, typeof threads>)
 
-  return (
-    <div className='max-w-full overflow-y-scroll max-h-[calc(100vh-120px)]'>
-        <div className='flex flex-col gap-2 p-4 pt-0'>
+    return (
+        <div className='max-w-full overflow-y-scroll max-h-[calc(100vh-120px)]'>
+            <div className='flex flex-col gap-2 p-4 pt-0'>
+                {Object.entries(groupedThreads ?? {}).map(([date, threads]) => {
+                    return <React.Fragment key={date}>
+                        <div className='text-xs font-medium text-muted-foreground mt-5 first:mt-0'>
+                            {date}
+                        </div>
+                        {threads.map((thread) => {
+                            return <button key={thread.id} className={cn('flex flex-col gap-2 items-start rounded-lg border p-3 text-left text-sm transition-all relative ')}>
+                                <div className='flex flex-col w-full gap-2 '>
+                                    <div className='flex items-center '>
+                                        <div className='flex items-center gap-2'>
+                                            <div className='font-semibold'>
+                                                {thread.emails.at(-1)?.from.name}
+                                            </div>
+                                        </div>
+                                        <div className={cn('ml-auto text-xs')}>
+                                            {formatDistanceToNow(thread.emails.at(-1)?.sentAt ?? new Date(), { addSuffix: true})}
+                                        </div>
+                                    </div>
+                                    <div className='text-xs font-medium'>
+                                        {thread.subject}
+                                    </div>
+                                </div>
+                                <div className='text-xs line-clamp-2 text-muted-foreground'
+                                dangerouslySetInnerHTML={{
+                                    __html: DOMPurify.sanitize(thread.emails.at(-1)?.bodySnippet ?? "", {
+                                        USE_PROFILES: {html: true}
+                                    })
+                                }}
+                                >
+                                </div>
+                                {thread.emails[0]?.sysLabels.length && (
+                                    <div className='flex items-center gap-2'>
+                                        {thread.emails[0]?.sysLabels.map(label => {
+                                            return <Badge key={label} variant={getBadgeVariantFromLabel(label)}>
+                                                {label}
+                                            </Badge>
+                                        })}
+                                    </div>
+                                )}
+                            </button>
+                        })}
+                    </React.Fragment>
+                })}
+            </div>
+
         </div>
-      
-    </div>
-  )
+    )
+}
+
+function getBadgeVariantFromLabel(label: string): ComponentProps<typeof Badge>['variant'] {
+    if (['work'].includes(label.toLowerCase())) {
+        return "default"
+    }
+    return 'secondary'
 }
 
 export default ThreadList
