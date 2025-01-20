1e76171929cf458a4dc78a7c5e7fcde1d93af150
awaisnazir08 <owaisnazir2228@gmail.com>
2025-01-20 18:47:47 +0500

    get the initials of thread mail sender name in threads tab

--- src/app/mail/thread-display.tsx
+++ src/app/mail/thread-display.tsx
@@ -12,6 +12,7 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 
 const ThreadDisplay = () => {
@@ -54,6 +55,25 @@ const ThreadDisplay = () => {
                 </div>
             </div>
             <Separator />
+            {thread ? 
+            <React.Fragment>
+                <div className='flex flex-col flex-1 overflow-scroll'>
+                    <div className='flex items-center p-4'>
+                        <div className='flex items-center gap-4 text-sm'>
+                            <Avatar> 
+                                <AvatarImage alt='avatar'/>
+                                <AvatarFallback>
+                                    {thread.emails[0]?.from?.name?.split(' ').map(chunk => chunk[0]).join('')}
+                                </AvatarFallback>
+                            </Avatar>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment> : <React.Fragment>
+                <div className='p-8 text-center text-muted-foreground'>
+                    No message selected
+                </div>
+                </React.Fragment>}
         </div>
     )
 }
