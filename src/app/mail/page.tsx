26f89967d502f08a08731ef9b4a2a8367330c8b8
awaisnazir08 <owaisnazir2228@gmail.com>
2025-01-20 11:02:28 +0500

    add ThemeToggle for button and change mode on the mail page.tsx

--- src/app/mail/page.tsx
+++ src/app/mail/page.tsx
@@ -1,3 +1,4 @@
+import ThemeToggle from '@/components/theme-toggle';
 import dynamic from 'next/dynamic'
 import React from 'react'
 // import Mail from './mail'
@@ -5,17 +6,21 @@ const Mail = dynamic(()=> {
   return import("./mail");
 }, {
   ssr: false
-}
-)
+})
 
 const MailDashboard = () => {
   return (
+    <React.Fragment >
+      <div className="absolute bottom-4 left-4">
+        <ThemeToggle />
+      </div>
     <Mail
     defaultLayout={[20,32,48]}
     defaultCollapsed = {false}
     navCollapsedSize={4}
     />
+    </React.Fragment>
   )
 }
 
-export default MailDashboard
+export default MailDashboard;
