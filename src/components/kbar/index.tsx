cd6897d5332cabeb34aa41254ed32582aba6b48b
awaisnazir08 <owaisnazir2228@gmail.com>
2025-01-22 11:47:31 +0500

    style the KBar

--- src/components/kbar/index.tsx
+++ src/components/kbar/index.tsx
@@ -2,7 +2,7 @@
 import { KBarAnimator, KBarPortal, KBarPositioner, KBarProvider, KBarSearch } from 'kbar';
 import React from 'react';
 
-export default function KBar ({ children }: { children: React.ReactNode }){  //children props from KBar to the ActualComponent
+export default function KBar({ children }: { children: React.ReactNode }) {  //children props from KBar to the ActualComponent
     return <KBarProvider>
         <ActualComponent>
             {children}
@@ -11,12 +11,16 @@ export default function KBar ({ children }: { children: React.ReactNode }){  //c
     </KBarProvider>
 }
 
-const ActualComponent = ({ children }: { children: React.ReactNode}) => {
+const ActualComponent = ({ children }: { children: React.ReactNode }) => {
     return <React.Fragment>
         <KBarPortal>
-            <KBarPositioner>
-                <KBarAnimator>
-                    <KBarSearch />
+            <KBarPositioner className='fixed inset-0 bg-black/40 dark:bg-black/60 backdrop-blur-sm scrollbar-hide !p-0 z-[999]'>
+                <KBarAnimator className='max-w-[600px] !mt-64 w-full bg-white dark:bg-gray-800 text-foreground dark:text-gray-200 shadow-lg border dark:border-gray-700 rounded-lg overflow-hidden relative !-translate-y-12'>
+                    <div className='bg-white dark:bg-gray-800'>
+                        <div className='border-x-0 border-b-2 dark:border-gray-700'>
+                            <KBarSearch className='py-4 px-6 text-lg w-full bg-white dark:bg-gray-800 outline-none border-none focus:outline-none focus:ring-0 focus:ring-offset-0' />
+                        </div>
+                    </div>
                 </KBarAnimator>
             </KBarPositioner>
         </KBarPortal>
