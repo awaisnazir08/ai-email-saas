35f06479043c7dc66b68aeba3fc062c0ae88a722
awaisnazir08 <owaisnazir2228@gmail.com>
2025-01-26 23:25:18 +0500

    build compose button using reusable email editor component

--- src/app/mail/page.tsx
+++ src/app/mail/page.tsx
@@ -1,6 +1,8 @@
 import ThemeToggle from '@/components/theme-toggle';
+import { UserButton } from '@clerk/nextjs';
 import dynamic from 'next/dynamic'
 import React from 'react'
+import ComposeButton from './compose-button';
 // import Mail from './mail'
 const Mail = dynamic(()=> {
   return import("./mail");
@@ -12,7 +14,11 @@ const MailDashboard = () => {
   return (
     <React.Fragment >
       <div className="absolute bottom-4 left-4">
+        <div className="flex items-center gap-2">
+        <UserButton />
         <ThemeToggle />
+        <ComposeButton />
+        </div>
       </div>
     <Mail
     defaultLayout={[20,32,48]}
