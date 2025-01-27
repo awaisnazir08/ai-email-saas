5e86e2b8cad807ffd803665349e554f12bd30d65
awaisnazir08 <owaisnazir2228@gmail.com>
2025-01-27 13:27:28 +0500

    create endpoint to send email messages

--- src/lib/account.ts
+++ src/lib/account.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { EmailMessage, SyncResponse, SyncUpdatedResponse } from "./types";
+import { EmailAddress, EmailMessage, SyncResponse, SyncUpdatedResponse } from "./types";
 
 export class Account {
     private token: string;
@@ -87,6 +87,62 @@ export class Account {
             else {
                 console.error("Error during sync: ", error)
             }
+        }
+    }
+
+    async sendEmail({
+        from,
+        subject,
+        body,
+        inReplyTo,
+        threadId,
+        references,
+        to,
+        cc,
+        bcc,
+        replyTo
+    }: {
+        from: EmailAddress,
+        subject: string,
+        body: string,
+        inReplyTo: string,
+        threadId?: string,
+        references: string,
+        to: EmailAddress[],
+        cc?: EmailAddress[],
+        bcc?: EmailAddress[],
+        replyTo?: EmailAddress
+    }) {
+        try {
+            const response = await axios.post('https://api.aurinko.io/v1/email/messages', {
+                from,
+                subject,
+                body, 
+                inReplyTo,
+                references,
+                to,
+                threadId,
+                cc,
+                bcc,
+                replyTo: [replyTo]
+            }, {
+                params: {
+                    returnIds: true
+                },
+                headers: {
+                    Authorization: `Bearer ${this.token}`,
+                },
+            })
+            console.log('Email sent', response.data)
+            return response.data
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                console.error('Error sending email: ', JSON.stringify(error.response?.data, null, 2));
+            }
+            else {
+                console.error('Error sending email: ', error)
+            }
+            throw error;
         }
     }
 }
