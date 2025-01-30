007a30baf5c845ce9d0d9af4fc59ed264cf761bb
awaisnazir08 <owaisnazir2228@gmail.com>
2025-01-31 01:35:06 +0500

    resolve compose button hydration error

--- src/app/mail/page.tsx
+++ src/app/mail/page.tsx
@@ -2,8 +2,15 @@ import ThemeToggle from '@/components/theme-toggle';
 import { UserButton } from '@clerk/nextjs';
 import dynamic from 'next/dynamic'
 import React from 'react'
-import ComposeButton from './compose-button';
+// import ComposeButton from './compose-button';
 // import Mail from './mail'
+
+const ComposeButton = dynamic(() => {
+  return import('./compose-button')
+}, {
+  ssr: false
+} )
+
 const Mail = dynamic(()=> {
   return import("./mail");
 }, {
