a638aaaac2cf1ce8a1d74599f49bae86fef13032
awaisnazir08 <owaisnazir2228@gmail.com>
2025-02-03 01:29:44 +0500

    complete api/chat endpoint

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,13 +1,18 @@
-import { Configuration } from 'openai-edge';
-import { Message, OpenAIStream, StreamingTextResponse } from 'ai';
+import { Configuration, OpenAIApi } from 'openai-edge';
+import { Message, streamText } from 'ai';
 import { auth } from '@clerk/nextjs/server';
 import { OramaClient } from '@/lib/orama';
+import { openai } from '@ai-sdk/openai';
+
+// const openai = new OpenAIApi(new Configuration({
+//     apiKey: process.env.OPENAI_API_KEY
+// }))
 
 export async function POST(req: Request) {
     try {
-        const {userId} = await auth();
+        const { userId } = await auth();
         if (!userId) {
-            return new Response('Unauthorized', {status: 401})
+            return new Response('Unauthorized', { status: 401 })
         }
         const { accountId, messages } = await req.json();
         const orama = new OramaClient(accountId)
@@ -15,9 +20,42 @@ export async function POST(req: Request) {
 
         const lastMessage = messages[messages.length - 1]
         console.log(lastMessage)
-        const context = await orama.vectorSearch({term: lastMessage.content})
-        console.log(context.hits.length + ' hits found')
-        return new Response('OK', {status: 200})
+        const context = await orama.vectorSearch({ term: lastMessage.content })
+        console.log(context.hits.length + ' hits found');
+
+        const prompt = {
+            role: "system",
+            content: `You are an AI email assistant embedded in an email client app. Your purpose is to help the user compose emails by answering questions, providing suggestions, and offering relevant information based on the context of their previous emails.
+            THE TIME NOW IS ${new Date().toLocaleString()}
+      
+      START CONTEXT BLOCK
+      ${context.hits.map((hit) => JSON.stringify(hit.document)).join('\n')}
+      END OF CONTEXT BLOCK
+      
+      When responding, please keep in mind:
+      - Be helpful, clever, and articulate.
+      - Rely on the provided email context to inform your responses.
+      - If the context does not contain enough information to answer a question, politely say you don't have enough information.
+      - Avoid apologizing for previous responses. Instead, indicate that you have updated your knowledge based on new information.
+      - Do not invent or speculate about anything that is not directly supported by the email context.
+      - Keep your responses concise and relevant to the user's questions or the email being composed.`
+        };
+
+        const response = streamText({
+            model: openai('gpt-3.5-turbo'),
+            messages: [prompt, ...messages.filter((message: Message) => message.role === 'user')],
+        })
+        // const stream = OpenAIStream(response, {
+        //     onStart: async () => {
+        //         console.log('hjgdf')
+        //     }, 
+        //     onCompletion: async (completion) => {
+        //         console.log('stream completed', completion)
+        //     },
+        //     onText: (text)
+        // })
+        return response.toDataStreamResponse();
+
     } catch (error) {
         console.log('error', error)
         return new Response("Internal Server Error", { status: 500 })
