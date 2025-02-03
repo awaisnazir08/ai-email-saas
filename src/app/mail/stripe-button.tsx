34c6ddc8ef71417607f3eb621013ca67377628a7
awaisnazir08 <owaisnazir2228@gmail.com>
2025-02-03 10:16:09 +0500

    create basic stripe-button component

---
+++ src/app/mail/stripe-button.tsx
@@ -0,0 +1,17 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import { createCheckoutSession } from '@/lib/stripe-actions';
+import React from 'react'
+
+const StripeButton = () => {
+    const handleClick = async () => {
+        await createCheckoutSession();
+    }
+  return (
+    <Button variant={'outline'} size={'lg'} onClick={handleClick}>
+      
+    </Button>
+  )
+}
+
+export default StripeButton
