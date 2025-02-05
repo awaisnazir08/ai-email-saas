ef359a14fb213be090a9e152f3eaccbfbe0188eb
awaisnazir08 <owaisnazir2228@gmail.com>
2025-02-05 15:33:27 +0500

    handle subscription in the stripe-button

--- src/app/mail/stripe-button.tsx
+++ src/app/mail/stripe-button.tsx
@@ -1,15 +1,29 @@
 'use client';
 import { Button } from '@/components/ui/button';
-import { createCheckoutSession } from '@/lib/stripe-actions';
-import React from 'react'
+import { createBillingPortalSession, createCheckoutSession, getSubscriptionStatus } from '@/lib/stripe-actions';
+import React, { useState } from 'react';
 
 const StripeButton = () => {
-    const handleClick = async () => {
-        await createCheckoutSession();
+  const [isSubscribed, setIsSubscribed] = useState(false);
+
+  React.useEffect(() => {
+    (async () => {
+      const subscriptionStatus = await getSubscriptionStatus();
+      setIsSubscribed(subscriptionStatus);
+    })
+  }, [])
+
+  const handleClick = async () => {
+    if (isSubscribed) {
+      await createBillingPortalSession()
+    } else {
+      await createCheckoutSession();
     }
+
+  }
   return (
     <Button variant={'outline'} size={'lg'} onClick={handleClick}>
-      
+      {isSubscribed ? 'Manage Subscription' : 'Upgrade Plan'}
     </Button>
   )
 }
